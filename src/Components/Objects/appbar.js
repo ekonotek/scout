277f7d5f5121e295cf2ba3f5702d287b0171eb55
ekonotek <ekonotek@gmail.com>
2020-11-18 14:58:25 -0600

    Copy Emm code for appbar

---
+++ src/Components/Objects/appbar.js
@@ -0,0 +1,112 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// import logo from "./logo.svg";
+// import "./App.css";
+// import { Box } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+import "typeface-roboto";
+
+import Container from "@material-ui/core/Container";
+import { fade, makeStyles } from "@material-ui/core/styles";
+
+import {
+  AppBar,
+  // Badge,
+  Box,
+  // List,
+MenuList,
+MenuItem,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: 100,
+    backgroundColor: fade(theme.palette.common.white, 0.95),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 1)
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(100),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "black"
+  },
+  inputRoot: {
+    color: "black"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch"
+      }
+    }
+  }
+}));
+
+
+export default function SearchAppbarr() {
+const classes = useStyles();
+return(
+<AppBar position="static" style={{ backgroundColor: "#cf5636" }}>
+<Toolbar>
+  <IconButton
+    edge="start"
+    className={classes.menuButton}
+    color="inherit"
+    aria-label="menu"
+  >
+    <MenuIcon />
+  </IconButton>
+  <Typography variant="h6" className={classes.title}>
+    ART SCOUTERS
+  </Typography>
+  <Button color="inherit">Login</Button>
+</Toolbar>
+</AppBar>
+)}
